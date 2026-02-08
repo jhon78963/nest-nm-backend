b6ff2086978ffc500f516963deed9bf744e6f80f
Jhon Livias <jhonlivias3@gmail.com>
2026-02-08 13:14:45 -0500

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -34,27 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
-      operationsSorter: (a: any, b: any) => {
-        const methodsOrder = [
-          'get',
-          'post',
-          'put',
-          'patch',
-          'delete',
-          'options',
-          'trace',
-        ];
-
-        let result =
-          methodsOrder.indexOf(a.get('method')) -
-          methodsOrder.indexOf(b.get('method'));
-
-        if (result === 0) {
-          result = a.get('path').localeCompare(b.get('path'));
-        }
-
-        return result;
-      },
+      operationsSorter: 'method',
     },
   });
 
