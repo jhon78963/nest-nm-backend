08c0b8e77da4d184a26e10983b344bf1d0a29312
Jhon Livias <jhonlivias3@gmail.com>
2026-02-09 20:43:56 -0500

    fix order swagger

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,27 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
-      operationsSorter: 'method',
+      operationsSorter: (a: any, b: any) => {
+        const methodsOrder = [
+          'get',
+          'post',
+          'put',
+          'patch',
+          'delete',
+          'options',
+          'trace',
+        ];
+
+        let result =
+          methodsOrder.indexOf(a.get('method')) -
+          methodsOrder.indexOf(b.get('method'));
+
+        if (result === 0) {
+          result = a.get('path').localeCompare(b.get('path'));
+        }
+
+        return result;
+      },
     },
   });
 
